846019c704a233fa4811f879dedd7ffd3d34bfea
Chris Köbke <c.koebke@hotmail.de>
2013-07-18 22:41:34 +0200

    Set up project.

---
+++ Xemio.Adventure/App/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Xemio.Adventure.App
+{
+    static class Program
+    {
+        /// <summary>
+        /// Main entrance point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new MainForm());
+        }
+    }
+}
