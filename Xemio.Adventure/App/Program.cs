cdea4d0cb8350952ba47a8aa1d34f69f5bc77ef6
Chris Köbke <c.koebke@hotmail.de>
2013-07-19 16:18:54 +0200

    Implemented splash screen.

--- Xemio.Adventure/App/Program.cs
+++ Xemio.Adventure/App/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Windows.Forms;
+using Xemio.Adventure.Scenes;
 using Xemio.GameLibrary;
+using Xemio.GameLibrary.Game.Scenes;
 using Xemio.GameLibrary.Rendering.GDIPlus;
 
 namespace Xemio.Adventure.App
@@ -20,6 +22,9 @@ namespace Xemio.Adventure.App
 
             XGL.Initialize(new GDIGraphicsInitializer());
             XGL.Run(mainForm.Handle, 400, 300, 60);
+
+            SceneManager sceneManager = XGL.Components.Get<SceneManager>();
+            sceneManager.Add(new SplashScreen());
 
             Application.Run(mainForm);
         }
